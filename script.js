15c46b1202b8f6bb62d51046f48a08378b63aba5
Bijoy Barman <bijoy20-127@aec.ac.in>
2026-01-23 00:54:02 +0530

    feat: implement basic snake movement using grid-based rendering

--- script.js
+++ script.js
@@ -6,8 +6,69 @@ const blockWidth = 30
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
 
-for(let i=0; i<rows*cols; i++) {
-    const block = document.createElement("div");
-    block.classList.add("block");
-    board.appendChild(block);
+const blocks = []
+const snake = [{
+    x:1, y:3
+}]
+
+let direction = 'right'
+
+// for(let i=0; i<rows*cols; i++) {
+//     const block = document.createElement("div");
+//     block.classList.add("block");
+//     board.appendChild(block);
+// }
+
+for(let row = 0; row < rows; row++){
+    for(let col = 0; col < cols; col++){
+        const block = document.createElement("div");
+        block.classList.add("block");
+        board.appendChild(block);
+        // block.innerText = `${row}-${col}`
+        blocks[`${row}-${col}`] = block
+    }
 }
+
+function render() {
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+    })
+}
+
+setInterval(() => {
+
+    let head = null
+
+    if (direction === 'left') {
+        head = {x: snake[0].x, y: snake[0].y-1}
+    } else if (direction === 'right') {
+        head = {x: snake[0].x, y: snake[0].y+1}
+    } else if (direction === 'down') {
+        head = {x: snake[0].x+1, y: snake[0].y}
+    } else if (direction === 'up') {
+        head = {x: snake[0].x-1, y: snake[0].y}
+    }
+
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
+    })
+
+    snake.unshift(head)
+    snake.pop()
+
+
+
+    render()
+}, 400);
+
+addEventListener("keydown", (event)=> {
+    if(event.key == "ArrowUp") {
+        direction = "up"
+    } else if (event.key == "ArrowDown") {
+        direction = "down"
+    } else if (event.key == "ArrowRight") {
+        direction = "right"
+    } else if (event.key == "ArrowLeft") {
+        direction = "left"
+    }
+})
